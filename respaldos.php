3ccf27d6711795e37fd55c054625db3607857bbc
Abigail Pineda Hernandez <abigailpinedah@gmail.com>
2026-03-10 16:08:02 -0600

    se agrego respaldos

---
+++ respaldos.php
@@ -0,0 +1,34 @@
+
+ <head>
+ </head>
+
+
+<body>
+    <header>
+         <!-- place naiber here--> 
+
+    </header>
+    ><main>
+        <a
+            name=""
+            id=""
+            class="btn btn-primary"
+            href="#"
+            role="button"
+             Generar respaldo
+
+            >Button</a
+        >
+        
+
+    
+        <a href="reportes.php"> Reporte mensual</a>
+        <br href="dashboard.ph">dashboard</a>
+
+        </main>
+        <footer>
+            <!-- place footer here--> 
+        </footer>
+ <!-- Bootstrap JavaScript Libraries--> 
+    <body>
+</html>
